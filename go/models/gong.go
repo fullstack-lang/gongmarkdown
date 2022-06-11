b0df3a7dc7871a0cd90d060f44e3566db387bba5
peugeot <thomaspeugeot@yahoo.fr>
2022-06-12 00:06:40 +0200

    new gongc

--- go/models/gong.go
+++ go/models/gong.go
@@ -1557,6 +1557,15 @@ func (stageStruct *StageStruct) CreateReverseMap_Row_Cells() (res map[*Cell]*Row
 }
 
 
+// Gongstruct is the type paramter for generated generic function that allows 
+// - access to staged instances
+// - navigation between staged instances by going backward association links between gongstruct
+// - full refactoring of Gongstruct identifiers / fields
+type Gongstruct interface {
+	// insertion point for generic types
+	AnotherDummyData | Cell | DummyData | Element | MarkdownContent | Row
+}
+
 type GongstructSet interface {
 	map[any]any |
 		// insertion point for generic types
@@ -1628,6 +1637,252 @@ func GongGetMap[Type GongstructMapString]() *Type {
 		return nil
 	}
 }
+
+// GetGongstructInstancesSet returns the set staged GongstructType instances
+// it is usefull because it allows refactoring of gongstruct identifier
+func GetGongstructInstancesSet[Type Gongstruct]() *map[*Type]any {
+	var ret Type
+
+	switch any(ret).(type) {
+	// insertion point for generic get functions
+	case AnotherDummyData:
+		return any(&Stage.AnotherDummyDatas).(*map[*Type]any)
+	case Cell:
+		return any(&Stage.Cells).(*map[*Type]any)
+	case DummyData:
+		return any(&Stage.DummyDatas).(*map[*Type]any)
+	case Element:
+		return any(&Stage.Elements).(*map[*Type]any)
+	case MarkdownContent:
+		return any(&Stage.MarkdownContents).(*map[*Type]any)
+	case Row:
+		return any(&Stage.Rows).(*map[*Type]any)
+	default:
+		return nil
+	}
+}
+
+// GetGongstructInstancesMap returns the map of staged GongstructType instances
+// it is usefull because it allows refactoring of gong struct identifier
+func GetGongstructInstancesMap[Type Gongstruct]() *map[string]*Type {
+	var ret Type
+
+	switch any(ret).(type) {
+	// insertion point for generic get functions
+	case AnotherDummyData:
+		return any(&Stage.AnotherDummyDatas_mapString).(*map[string]*Type)
+	case Cell:
+		return any(&Stage.Cells_mapString).(*map[string]*Type)
+	case DummyData:
+		return any(&Stage.DummyDatas_mapString).(*map[string]*Type)
+	case Element:
+		return any(&Stage.Elements_mapString).(*map[string]*Type)
+	case MarkdownContent:
+		return any(&Stage.MarkdownContents_mapString).(*map[string]*Type)
+	case Row:
+		return any(&Stage.Rows_mapString).(*map[string]*Type)
+	default:
+		return nil
+	}
+}
+
+// GetAssociationName is a generic function that returns an instance of Type
+// where each association is filled with an instance whose name is the name of the association
+//
+// This function can be handy for generating navigation function that are refactorable
+func GetAssociationName[Type Gongstruct]() *Type {
+	var ret Type
+
+	switch any(ret).(type) {
+	// insertion point for instance with special fields
+	case AnotherDummyData:
+		return any(&AnotherDummyData{
+			// Initialisation of associations
+		}).(*Type)
+	case Cell:
+		return any(&Cell{
+			// Initialisation of associations
+		}).(*Type)
+	case DummyData:
+		return any(&DummyData{
+			// Initialisation of associations
+			// field is initialized with an instance of AnotherDummyData with the name of the field
+			DummyPointerToGongStruct: &AnotherDummyData{Name: "DummyPointerToGongStruct"},
+		}).(*Type)
+	case Element:
+		return any(&Element{
+			// Initialisation of associations
+			// field is initialized with an instance of Element with the name of the field
+			SubElements: []*Element{{Name: "SubElements"}},
+			// field is initialized with an instance of Row with the name of the field
+			Rows: []*Row{{Name: "Rows"}},
+		}).(*Type)
+	case MarkdownContent:
+		return any(&MarkdownContent{
+			// Initialisation of associations
+			// field is initialized with an instance of Element with the name of the field
+			Root: &Element{Name: "Root"},
+		}).(*Type)
+	case Row:
+		return any(&Row{
+			// Initialisation of associations
+			// field is initialized with an instance of Cell with the name of the field
+			Cells: []*Cell{{Name: "Cells"}},
+		}).(*Type)
+	default:
+		return nil
+	}
+}
+
+// GetPointerReverseMap allows backtrack navigation of any Start.Fieldname
+// associations (0..1) that is a pointer from one staged Gongstruct (type Start)
+// instances to another (type End)
+//
+// The function provides a map with keys as instances of End and values to arrays of *Start
+// the map is construed by iterating over all Start instances and populationg keys with End instances
+// and values with slice of Start instances
+func GetPointerReverseMap[Start, End Gongstruct](fieldname string) map[*End][]*Start {
+	var ret Start
+
+	switch any(ret).(type) {
+	// insertion point of functions that provide maps for reverse associations
+	// reverse maps of direct associations of AnotherDummyData
+	case AnotherDummyData:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of Cell
+	case Cell:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of DummyData
+	case DummyData:
+		switch fieldname {
+		// insertion point for per direct association field
+		case "DummyPointerToGongStruct":
+			res := make(map[*AnotherDummyData][]*DummyData)
+			for dummydata := range Stage.DummyDatas {
+				if dummydata.DummyPointerToGongStruct != nil {
+					anotherdummydata_ := dummydata.DummyPointerToGongStruct
+					var dummydatas []*DummyData
+					_, ok := res[anotherdummydata_]
+					if ok {
+						dummydatas = res[anotherdummydata_]
+					} else {
+						dummydatas = make([]*DummyData, 0)
+					}
+					dummydatas = append(dummydatas, dummydata)
+					res[anotherdummydata_] = dummydatas
+				}
+			}
+			return any(res).(map[*End][]*Start)
+		}
+	// reverse maps of direct associations of Element
+	case Element:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of MarkdownContent
+	case MarkdownContent:
+		switch fieldname {
+		// insertion point for per direct association field
+		case "Root":
+			res := make(map[*Element][]*MarkdownContent)
+			for markdowncontent := range Stage.MarkdownContents {
+				if markdowncontent.Root != nil {
+					element_ := markdowncontent.Root
+					var markdowncontents []*MarkdownContent
+					_, ok := res[element_]
+					if ok {
+						markdowncontents = res[element_]
+					} else {
+						markdowncontents = make([]*MarkdownContent, 0)
+					}
+					markdowncontents = append(markdowncontents, markdowncontent)
+					res[element_] = markdowncontents
+				}
+			}
+			return any(res).(map[*End][]*Start)
+		}
+	// reverse maps of direct associations of Row
+	case Row:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	}
+	return nil
+}
+
+// GetSliceOfPointersReverseMap allows backtrack navigation of any Start.Fieldname
+// associations (0..N) between one staged Gongstruct instances and many others
+//
+// The function provides a map with keys as instances of End and values to *Start instances
+// the map is construed by iterating over all Start instances and populating keys with End instances
+// and values with the Start instances
+func GetSliceOfPointersReverseMap[Start, End Gongstruct](fieldname string) map[*End]*Start {
+	var ret Start
+
+	switch any(ret).(type) {
+	// insertion point of functions that provide maps for reverse associations
+	// reverse maps of direct associations of AnotherDummyData
+	case AnotherDummyData:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of Cell
+	case Cell:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of DummyData
+	case DummyData:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of Element
+	case Element:
+		switch fieldname {
+		// insertion point for per direct association field
+		case "SubElements":
+			res := make(map[*Element]*Element)
+			for element := range Stage.Elements {
+				for _, element_ := range element.SubElements {
+					res[element_] = element
+				}
+			}
+			return any(res).(map[*End]*Start)
+		case "Rows":
+			res := make(map[*Row]*Element)
+			for element := range Stage.Elements {
+				for _, row_ := range element.Rows {
+					res[row_] = element
+				}
+			}
+			return any(res).(map[*End]*Start)
+		}
+	// reverse maps of direct associations of MarkdownContent
+	case MarkdownContent:
+		switch fieldname {
+		// insertion point for per direct association field
+		}
+	// reverse maps of direct associations of Row
+	case Row:
+		switch fieldname {
+		// insertion point for per direct association field
+		case "Cells":
+			res := make(map[*Cell]*Row)
+			for row := range Stage.Rows {
+				for _, cell_ := range row.Cells {
+					res[cell_] = row
+				}
+			}
+			return any(res).(map[*End]*Start)
+		}
+	}
+	return nil
+}
+
 
 // insertion point of enum utility functions
 // Utility function for DummnyTypeInt
