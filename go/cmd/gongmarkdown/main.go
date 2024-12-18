2bf76986170159d1f7fc6423bfa41cc5dd3cd947
peugeot <thomaspeugeot@yahoo.fr>
2024-12-18 08:41:14 +0100

    tests with images

--- go/cmd/gongmarkdown/main.go
+++ go/cmd/gongmarkdown/main.go
@@ -58,6 +58,15 @@ This is a **standalone** Angular component using "ngx-markdown"
 
 - List item 1
 - List item 2
+
+![Local Angular Logo](assets/images/image.png)
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100" height="100" fill="gold">
+  <polygon points="12,2 15.09,8.26 22,9.27 17,14.14 18.18,21.02 12,17.77 5.82,21.02 7,14.14 2,9.27 8.91,8.26" />
+</svg>
+
+<img src=/assets/images/star.svg width=50 />
+
 `
 		markdownContent.Root = root
 
